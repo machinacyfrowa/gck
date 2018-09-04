2241793f74133874a41d851cd5eeb00dbb4ff238
pawcio <darker300@o2.pl>
2018-09-04 10:52:03 +0200

    Działa
    aczkolwiek można wprowadzić kilka drobnych poprawek...

--- JS/js/text.js
+++ JS/js/text.js
@@ -8,17 +8,37 @@ $( window ).ready(function() {
 });
 
 function graduation(){
+
+    var maxHeight = 0;
+    $( ".event-title" ).css("height","auto");
+    $(".event-title").each(function() {
+      if ($(this).outerHeight() > maxHeight) {
+        maxHeight = $(this).outerHeight();
+      }
+    }).height(maxHeight);
+
+    maxHeight = 0;
+    $( ".event-grid p" ).css("height","auto");
+    $(".event-grid p").each(function() {
+      if ($(this).outerHeight() > maxHeight) {
+        maxHeight = $(this).outerHeight();
+      }
+    }).height(maxHeight);
+    /*
     var lastHight = -1;
     var maxHight =  -1;
     var mainElement = $(".event-grid:first>div").length;
     var stop = false; 
+    $( ".event-title" ).css("height","auto");
+    $( ".event-grid p" ).css("height","auto");
     $( ".event-title" ).each(function( i ) {
-        if((i+1) > mainElement && stop === false ){lastHight =-1;stop = true; }
-        if(lastHight >= $(this).height()){
+        //if((i+1) > mainElement && stop === false ){lastHight =-1;stop = true; }
+        if(lastHight > $(this).outerHeight()){
             maxHight = lastHight;
+            console.log("JEST :"+ maxHight);
         }
-        console.log("test "+ $(this).height());
-        lastHight =  $(this).height();
+        console.log("test "+ $(this).outerHeight());
+        lastHight =  $(this).outerHeight();
     });
     $(".event-title").height(maxHight);
     lastHight = -1;
@@ -32,4 +52,6 @@ function graduation(){
         lastHight =  $(this).height();
     });
     $(".event-title").height(lastHight);
+
+    */
 }
